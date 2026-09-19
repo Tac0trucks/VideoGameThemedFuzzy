ed3f0cf9a69ec9ae97dbedc32a411be2f5525741
Tac0trucks <abainciajoelconstantine@gmail.com>
2026-09-19 11:29:06 +0800

    Added the missing stuff

--- FuzzyLogicAct/Program.cs
+++ FuzzyLogicAct/Program.cs
@@ -8,8 +8,12 @@ namespace FuzzyLogicAct
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
